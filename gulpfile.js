8919fcfe934de376c340a33c221c8b873f1ecf51
Greg Tyler <git@gregtyler.co.uk>
2019-09-25 17:22:25 +0100

    Upgrade vulnerable packages

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('build', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('src/**/*', ['build']);
+  gulp.watch('src/**/*', gulp.series(['build']));
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', gulp.parallel('build', 'watch'));
