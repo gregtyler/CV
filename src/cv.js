ebfbf7aef3098feab14cd7d2158b6befadbd1338
Greg Tyler <gregtyler@users.noreply.github.com>
2023-07-05 14:28:57 +0100

    rv analytics

--- src/cv.js
+++ src/cv.js
@@ -1,11 +1,1 @@
-// Just Analytics
-var _paq = _paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="//analytics.gregtyler.co.uk/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', '4']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
+//Currently empty
